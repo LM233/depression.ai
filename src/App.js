aa1b49e7e2fe8ca8f343dcbeea2cd0767f6dc26a
LM233 <13141424853@qq.com>
2021-10-08 23:26:32 -0400

    First commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { Helmet } from "react-helmet";
+import { MDCTextField } from "@material/textfield";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const textField = new MDCTextField(
+      document.querySelector(".mdc-text-field")
+    );
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Helmet>
+        <script
+          type="text/javascript"
+          src="/live2d-widget/autoload.js"
+        ></script>
+      </Helmet>
+      <div class="input">
+        <label class="mdc-text-field mdc-text-field--filled">
+          <span class="mdc-text-field__ripple"></span>
+          <span class="mdc-floating-label" id="my-label-id">
+            Hint text
+          </span>
+          <input
+            class="mdc-text-field__input"
+            type="text"
+            aria-labelledby="my-label-id"
+          ></input>
+          <span class="mdc-line-ripple"></span>
+        </label>
+      </div>
     </div>
   );
 }
